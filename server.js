4210f1d2cf80c7be52df768aec8add05b5c361c6
miracleibanze <miraclecode11@gmail.com>
2024-11-19 20:44:05 +0200

    ready to deploy

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const session = require("express-session");
 const cors = require("cors");
 const passport = require("passport");
 const mongoose = require("mongoose");
+const Redis = require("redis");
+const connectRedis = require("connect-redis");
 const User = require("./models/User");
 const app = express();
-const PORT = process.env.PORT || 3000;
 const http = require("http");
 const { Server } = require("socket.io");
 
@@ -14,8 +15,9 @@ const { corsOptions, socketCors } = require("./config/cors");
 const { getAllMessages } = require("./controllers/messageControllers");
 const { initialize } = require("./socket");
 
+// Set up MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -25,13 +27,23 @@ mongoose
     process.exit(1);
   });
 
+// Set up Redis client
+const RedisStore = connectRedis(session);
+const redisClient = Redis.createClient({
+  host: process.env.REDIS_HOST, // You will need to set up a Redis server (use Redis Cloud or your own instance)
+  port: process.env.REDIS_PORT,
+});
+
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
+// Enable CORS
 app.use(cors(corsOptions));
 
+// Use Redis for session storage
 app.use(
   session({
+    store: new RedisStore({ client: redisClient }),
     secret: "miraclecode11",
     resave: false,
     saveUninitialized: false,
@@ -39,9 +51,11 @@ app.use(
   })
 );
 
+// Initialize Passport for authentication
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Define routes
 app.get("/", async (req, res) => {
   await User.updateMany(
     { createdAt: { $exists: false } },
@@ -55,12 +69,15 @@ app.get("/", async (req, res) => {
 
 app.use("/", require("./Routes/routes"));
 
+// Initialize socket.io
 const server = http.createServer(app);
-
 initialize(server);
 
-server.listen(5000, () => {
-  console.log("Socket running on port 5000");
+// Bind the server to listen on the environment's port or default to 5000
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Socket running on port 5000`);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
