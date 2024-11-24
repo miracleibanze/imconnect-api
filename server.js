1f0c2013f7d7ecd2f607f8cd2f84e6079e9364f0
miracleibanze <miraclecode11@gmail.com>
2024-11-24 09:50:08 +0200

    added vercel global

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { corsOptions } = require("./config/cors");
 const { initialize } = require("./socket");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 mongoose
   .connect(process.env.MONGODB_URL)
